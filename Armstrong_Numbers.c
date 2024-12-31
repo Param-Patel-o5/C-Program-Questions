cc4e9b87c529fd92686810004a9aa092a725971b
Param-Patel-o5 <parampatel.005@gmail.com>
2024-12-31 07:33:45 +0530

    code for Armstrong Number Problem

---
+++ Armstrong_Numbers.c
@@ -0,0 +1,39 @@
+// to check whether the given number is ARMSTRONG or not
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int number, count = 0;
+    printf("PLease enter the Number: ");
+    scanf("%d", &number);
+
+    int Q = number;
+
+    while (Q != 0)
+    {
+        Q = Q / 10;
+        count++;
+    }
+
+    int remainder, sum = 0, P;
+    P = number;
+
+    while (P != 0)
+    {
+        remainder = P % 10;
+
+        sum = sum + (int)round(pow(remainder, count));
+
+        P = P / 10;
+    }
+
+    if (sum == number)
+    {
+        printf("Number is Armstrong Number");
+    }
+    else
+    {
+        printf("No! Number is not an Armstrong Number ");
+    }
+}
